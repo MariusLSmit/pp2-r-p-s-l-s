873b7cf5ee8a6fa27203d826564377c1f3ff4ba6
MariusLSmit <msmit986@gmail.com>
2022-09-26 17:04:13 +0000

    added main function to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,28 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
 const choices = ["rock", "paper", "scissors"];
+
+//Event listeners for buttons to loop through
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    })
+}
+
+//Main game function
+
+function playGame(playerChoice){
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+
+    let computerChoice = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(result);
+}
+
+//Finds the winner
