8eacad3ff94270c5f5072846bdc6679d7a0749a3
MariusLSmit <msmit986@gmail.com>
2022-09-26 10:54:35 +0000

    removed all js code due to plan not working, added aria labels and data choices to buttons

--- assets/js/script.js
+++
@@ -1,3 +0,0 @@
-const yourChoice = document.getElementById('your-choice')
-const computerChoice = document.getElementById('computer-choice')
-const endresultDisplay = document.getElementById('end-result')
